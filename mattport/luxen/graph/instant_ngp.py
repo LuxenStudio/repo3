11d4c1a451893cf9347b0c0fd8b600afceea9f0a
luxemstudio <kalijunaidtest@gmail.com>
2024-09-18 07:33:06 -0700

    Factor out luxen_field

--- mattport/luxen/graph/instant_ngp.py
+++ mattport/luxen/graph/instant_ngp.py
@@ -2,33 +2,8 @@
 Implementation of Instant NGP.
 """
 
-from torch import nn
-
-from mattport.luxen.field_modules.encoding import HashEncoding, LuxenEncoding
-from mattport.luxen.field_modules.mlp import MLP
+from mattport.luxen.field_modules.encoding import HashEncoding, SHEncoding
 from mattport.luxen.graph.vanilla_luxen import LuxenField, LuxenGraph
-
-
-class NGPField(LuxenField):
-    """Luxen module"""
-
-    def __init__(self, num_layers=3, layer_width=64) -> None:
-        super().__init__(num_layers=num_layers, layer_width=layer_width)
-
-    def build_encodings(self):
-        self.encoding_xyz = HashEncoding()
-        self.encoding_dir = LuxenEncoding(
-            in_dim=3, num_frequencies=4, min_freq_exp=0.0, max_freq_exp=4.0, include_input=True
-        )
-
-    def build_mlp_base(self):
-        self.mlp_base = MLP(
-            in_dim=self.encoding_xyz.get_out_dim(),
-            out_dim=self.layer_width,
-            num_layers=self.num_layers,
-            layer_width=self.layer_width,
-            activation=nn.ReLU(),
-        )
 
 
 class NGPGraph(LuxenGraph):
@@ -39,5 +14,23 @@ class NGPGraph(LuxenGraph):
 
     def populate_fields(self):
         """Set the fields."""
-        self.field_coarse = NGPField()
-        self.field_fine = NGPField()
+
+        position_encoding = HashEncoding()
+        direction_encoding = SHEncoding()
+
+        self.field_coarse = LuxenField(
+            position_encoding=position_encoding,
+            direction_encoding=direction_encoding,
+            base_mlp_num_layers=3,
+            base_mlp_layer_width=64,
+            head_mlp_num_layers=2,
+            head_mlp_layer_width=32,
+        )
+        self.field_fine = LuxenField(
+            position_encoding=position_encoding,
+            direction_encoding=direction_encoding,
+            base_mlp_num_layers=3,
+            base_mlp_layer_width=64,
+            head_mlp_num_layers=2,
+            head_mlp_layer_width=32,
+        )
