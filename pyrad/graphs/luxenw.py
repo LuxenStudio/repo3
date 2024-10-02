9da8a24190d8df084ea7dee27c2c1595f3b87662
luxemstudio <kalijunaidtest@gmail.com>
2024-10-02 07:05:29 -0700

    Fix SSIM memory leak

--- pyrad/graphs/luxenw.py
+++ pyrad/graphs/luxenw.py
@@ -17,8 +17,9 @@ Luxen-W (Luxen in the wild) implementation.
 """
 
 import torch
-from torchmetrics import PeakSignalNoiseRatio, StructuralSimilarityIndexMeasure
+from torchmetrics import PeakSignalNoiseRatio
 from torchmetrics.image.lpip import LearnedPerceptualImagePatchSimilarity
+from torchmetrics.functional import structural_similarity_index_measure
 
 from pyrad.fields.modules.encoding import LuxenEncoding
 from pyrad.fields.modules.field_heads import FieldHeadNames
@@ -92,7 +93,7 @@ class LuxenWGraph(Graph):
 
         # metrics
         self.psnr = PeakSignalNoiseRatio(data_range=1.0)
-        self.ssim = StructuralSimilarityIndexMeasure()
+        self.ssim = structural_similarity_index_measure
         self.lpips = LearnedPerceptualImagePatchSimilarity()
 
     def get_param_groups(self):
