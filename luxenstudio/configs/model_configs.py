667660f51932309e1ca1c21fed32d9912ac9316f
luxemstudio <kalijunaidtest@gmail.com>
2024-10-29 14:58:42 -0700

    use literal instead of list for --vis (#628)
    use literal instead of list

--- luxenstudio/configs/model_configs.py
+++ luxenstudio/configs/model_configs.py
@@ -74,7 +74,7 @@ model_configs["luxenacto"] = Config(
         },
     },
     viewer=ViewerConfig(num_rays_per_chunk=1 << 16),
-    vis=["viewer"],
+    vis="viewer",
 )
 
 model_configs["instant-ngp"] = Config(
@@ -91,7 +91,7 @@ model_configs["instant-ngp"] = Config(
         }
     },
     viewer=ViewerConfig(num_rays_per_chunk=64000),
-    vis=["viewer"],
+    vis="viewer",
 )
 
 model_configs["mipluxen-360"] = Config(
