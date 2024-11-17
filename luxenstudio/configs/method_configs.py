ad82c66f3d188e6a03bd12cc0bc761fe538fe90f
luxemstudio <kalijunaidtest@gmail.com>
2024-11-16 19:50:19 -0500

    limit semantic-luxenw iterations to 30000 (#1074)
    Per discussion at https://github.com/luxenstudio-project/luxenstudio/issues/1072#issuecomment-1334644896

--- luxenstudio/configs/method_configs.py
+++ luxenstudio/configs/method_configs.py
@@ -135,7 +135,7 @@ method_configs["mipluxen"] = Config(
 
 method_configs["semantic-luxenw"] = Config(
     method_name="semantic-luxenw",
-    trainer=TrainerConfig(steps_per_eval_batch=500, steps_per_save=2000, mixed_precision=True),
+    trainer=TrainerConfig(steps_per_eval_batch=500, steps_per_save=2000, max_num_iterations=30000, mixed_precision=True),
     pipeline=VanillaPipelineConfig(
         datamanager=SemanticDataManagerConfig(
             dataparser=FriendsDataParserConfig(), train_num_rays_per_batch=4096, eval_num_rays_per_batch=8192
