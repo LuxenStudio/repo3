f6b7cda4bfc5a97337e3aa02f6a12b8361f5fa71
luxemstudio <kalijunaidtest@gmail.com>
2024-12-07 22:56:32 -0500

    make robust and patch_size model params (#1386)
    * make robust and patch_size model params
    
    * change defaults back, turns off robust by default
    
    * Make luxenacto-robust a separate config using same model

--- luxenstudio/configs/method_configs.py
+++ luxenstudio/configs/method_configs.py
@@ -83,13 +83,51 @@ method_configs["luxenacto"] = TrainerConfig(
     pipeline=VanillaPipelineConfig(
         datamanager=VanillaDataManagerConfig(
             dataparser=LuxenstudioDataParserConfig(),
+            train_num_rays_per_batch=4096,
+            eval_num_rays_per_batch=4096,
+            camera_optimizer=CameraOptimizerConfig(
+                mode="SO3xR3", optimizer=AdamOptimizerConfig(lr=6e-4, eps=1e-8, weight_decay=1e-2)
+            ),
+        ),
+        model=LuxenactoModelConfig(
+            eval_num_rays_per_chunk=1 << 15,
+            robust=False,
+        ),
+    ),
+    optimizers={
+        "proposal_networks": {
+            "optimizer": AdamOptimizerConfig(lr=1e-2, eps=1e-15),
+            "scheduler": None,
+        },
+        "fields": {
+            "optimizer": AdamOptimizerConfig(lr=1e-2, eps=1e-15),
+            "scheduler": None,
+        },
+    },
+    viewer=ViewerConfig(num_rays_per_chunk=1 << 15),
+    vis="viewer",
+)
+
+method_configs["luxenacto-robust"] = TrainerConfig(
+    method_name="luxenacto-robust",
+    steps_per_eval_batch=500,
+    steps_per_save=2000,
+    max_num_iterations=30000,
+    mixed_precision=True,
+    pipeline=VanillaPipelineConfig(
+        datamanager=VanillaDataManagerConfig(
+            dataparser=LuxenstudioDataParserConfig(),
             train_num_rays_per_batch=17000,
             eval_num_rays_per_batch=4096,
             camera_optimizer=CameraOptimizerConfig(
                 mode="SO3xR3", optimizer=AdamOptimizerConfig(lr=6e-4, eps=1e-8, weight_decay=1e-2)
             ),
         ),
-        model=LuxenactoModelConfig(eval_num_rays_per_chunk=1 << 15),
+        model=LuxenactoModelConfig(
+            eval_num_rays_per_chunk=1 << 15,
+            robust=True,
+            patch_size=16
+        ),
     ),
     optimizers={
         "proposal_networks": {
