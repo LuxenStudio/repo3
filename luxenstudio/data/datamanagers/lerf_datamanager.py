25021845119d65638667c838ab8d253503e7674b
luxemstudio <kalijunaidtest@gmail.com>
2024-12-20 16:16:45 +0000

    initial lerf-merge

---
+++ luxenstudio/data/datamanagers/lerf_datamanager.py
@@ -0,0 +1,296 @@
+# Copyright 2022 The Luxenstudio Team. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Datamanager.
+"""
+
+from __future__ import annotations
+
+from abc import abstractmethod
+from dataclasses import dataclass, field
+from pathlib import Path
+from typing import Any, Dict, List, Optional, Tuple, Type, Union
+
+import torch
+import tyro
+from rich.progress import Console
+from torch import nn
+from torch.nn import Parameter
+from torch.utils.data import Dataset
+from torch.utils.data.distributed import DistributedSampler
+from typing_extensions import Literal
+
+from luxenstudio.cameras.camera_optimizers import CameraOptimizerConfig
+from luxenstudio.cameras.cameras import CameraType
+from luxenstudio.cameras.rays import RayBundle
+from luxenstudio.configs.base_config import InstantiateConfig
+from luxenstudio.data.dataparsers.arkitscenes_dataparser import (
+    ARKitScenesDataParserConfig,
+)
+from luxenstudio.data.dataparsers.base_dataparser import DataparserOutputs
+from luxenstudio.data.dataparsers.blender_dataparser import BlenderDataParserConfig
+from luxenstudio.data.dataparsers.dluxen_dataparser import DLuxenDataParserConfig
+from luxenstudio.data.dataparsers.dycheck_dataparser import DycheckDataParserConfig
+from luxenstudio.data.dataparsers.instant_ngp_dataparser import (
+    InstantNGPDataParserConfig,
+)
+from luxenstudio.data.dataparsers.minimal_dataparser import MinimalDataParserConfig
+from luxenstudio.data.dataparsers.luxenosr_dataparser import LuxenOSRDataParserConfig
+from luxenstudio.data.dataparsers.luxenstudio_dataparser import LuxenstudioDataParserConfig
+from luxenstudio.data.dataparsers.nuscenes_dataparser import NuScenesDataParserConfig
+from luxenstudio.data.dataparsers.phototourism_dataparser import (
+    PhototourismDataParserConfig,
+)
+from luxenstudio.data.dataparsers.scannet_dataparser import ScanNetDataParserConfig
+from luxenstudio.data.dataparsers.sdfstudio_dataparser import SDFStudioDataParserConfig
+from luxenstudio.data.dataparsers.sitcoms3d_dataparser import Sitcoms3DDataParserConfig
+from luxenstudio.data.datasets.base_dataset import InputDataset
+from luxenstudio.data.pixel_samplers import (
+    EquirectangularPixelSampler,
+    PatchPixelSampler,
+    PixelSampler,
+)
+from luxenstudio.data.utils.dataloaders import (
+    CacheDataloader,
+    FixedIndicesEvalDataloader,
+    RandIndicesEvalDataloader,
+)
+from luxenstudio.data.utils.luxenstudio_collate import luxenstudio_collate
+from luxenstudio.engine.callbacks import TrainingCallback, TrainingCallbackAttributes
+from luxenstudio.model_components.ray_generators import RayGenerator
+from luxenstudio.utils.misc import IterableWrapper
+
+CONSOLE = Console(width=120)
+
+from luxenstudio.data.datamanagers.base_datamanager import (
+    DataManager,
+    DataManagerConfig,
+    VanillaDataManagerConfig,
+    VanillaDataManager
+)
+
+from clip_luxen.clip_dataloader import CLIPDataloader
+from clip_luxen.dino_dataloader import DinoDataloader
+
+@dataclass
+class LERFDataManagerConfig(VanillaDataManagerConfig):
+    _target: Type = field(default_factory=lambda: LERFDataManager)
+
+class LERFDataManager(VanillaDataManager):  # pylint: disable=abstract-method
+    """Basic stored data manager implementation.
+
+    This is pretty much a port over from our old dataloading utilities, and is a little jank
+    under the hood. We may clean this up a little bit under the hood with more standard dataloading
+    components that can be strung together, but it can be just used as a black box for now since
+    only the constructor is likely to change in the future, or maybe passing in step number to the
+    next_train and next_eval functions.
+
+    Args:
+        config: the DataManagerConfig used to instantiate class
+    """
+
+    config: LERFDataManagerConfig
+
+    def __init__(
+        self,
+        config: VanillaDataManagerConfig,
+        device: Union[torch.device, str] = "cpu",
+        test_mode: Literal["test", "val", "inference"] = "val",
+        world_size: int = 1,
+        local_rank: int = 0,
+        **kwargs,  # pylint: disable=unused-argument
+    ):
+        self.config = config
+        self.device = device
+        self.world_size = world_size
+        self.local_rank = local_rank
+        self.sampler = None
+        self.test_mode = test_mode
+        self.test_split = "test" if test_mode in ["test", "inference"] else "val"
+        self.dataparser_config = self.config.dataparser
+        if self.config.data is not None:
+            self.config.dataparser.data = Path(self.config.data)
+        else:
+            self.config.data = self.config.dataparser.data
+        self.dataparser = self.dataparser_config.setup()
+        self.train_dataparser_outputs = self.dataparser.get_dataparser_outputs(split="train")
+
+        self.train_dataset = self.create_train_dataset()
+        self.eval_dataset = self.create_eval_dataset()
+        DataManager.__init__(self)
+
+    def create_train_dataset(self) -> InputDataset:
+        """Sets up the data loaders for training"""
+        return InputDataset(
+            dataparser_outputs=self.train_dataparser_outputs,
+            scale_factor=self.config.camera_res_scale_factor,
+        )
+
+    def create_eval_dataset(self) -> InputDataset:
+        """Sets up the data loaders for evaluation"""
+        return InputDataset(
+            dataparser_outputs=self.dataparser.get_dataparser_outputs(split=self.test_split),
+            scale_factor=self.config.camera_res_scale_factor,
+        )
+
+    def _get_pixel_sampler(  # pylint: disable=no-self-use
+        self, dataset: InputDataset, *args: Any, **kwargs: Any
+    ) -> PixelSampler:
+        """Infer pixel sampler to use."""
+        if self.config.patch_size > 1:
+            return PatchPixelSampler(*args, **kwargs, patch_size=self.config.patch_size)
+
+        # If all images are equirectangular, use equirectangular pixel sampler
+        is_equirectangular = dataset.cameras.camera_type == CameraType.EQUIRECTANGULAR.value
+        if is_equirectangular.all():
+            return EquirectangularPixelSampler(*args, **kwargs)
+        # Otherwise, use the default pixel sampler
+        if is_equirectangular.any():
+            CONSOLE.print("[bold yellow]Warning: Some cameras are equirectangular, but using default pixel sampler.")
+        return PixelSampler(*args, **kwargs)
+
+    def setup_train(self):
+        """Sets up the data loaders for training"""
+        assert self.train_dataset is not None
+        CONSOLE.print("Setting up training dataset...")
+        self.train_image_dataloader = CacheDataloader(
+            self.train_dataset,
+            num_images_to_sample_from=self.config.train_num_images_to_sample_from,
+            num_times_to_repeat_images=self.config.train_num_times_to_repeat_images,
+            device=self.device,
+            num_workers=self.world_size * 4,
+            pin_memory=True,
+            collate_fn=self.config.collate_fn,
+        )
+        self.iter_train_image_dataloader = iter(self.train_image_dataloader)
+        self.train_pixel_sampler = self._get_pixel_sampler(self.train_dataset, self.config.train_num_rays_per_batch)
+        self.train_camera_optimizer = self.config.camera_optimizer.setup(
+            num_cameras=self.train_dataset.cameras.size, device=self.device
+        )
+        self.train_ray_generator = RayGenerator(
+            self.train_dataset.cameras.to(self.device),
+            self.train_camera_optimizer,
+        )
+
+        images = [self.train_dataset[i]["image"].permute(2, 0, 1)[None, ...] for i in range(len(self.train_dataset))]
+        images = torch.cat(images)
+        self.clip_interpolator = CLIPDataloader(
+            image_list=images,
+            device=self.device,
+            cfg={
+                "tile_size_range": (0.05, 0.5),
+                "tile_size_res": 7,
+                "stride_scaler": 0.5,
+            },
+        )
+        self.clip_interpolator.try_load(f"outputs/{self.config.dataparser.data.name}")
+        # self.clip_interpolator.create()
+        # self.clip_interpolator.save(f"outputs/{self.config.dataparser.data.name}")
+        self.dino_dataloader = DinoDataloader(image_list=images, device=self.device, cfg={})
+        self.dino_dataloader.try_load(f"outputs/{self.config.dataparser.data.name}")
+        # self.dino_dataloader.create()
+        # self.dino_dataloader.save(f"outputs/{self.config.dataparser.data.name}")
+
+    def setup_eval(self):
+        """Sets up the data loader for evaluation"""
+        assert self.eval_dataset is not None
+        CONSOLE.print("Setting up evaluation dataset...")
+        self.eval_image_dataloader = CacheDataloader(
+            self.eval_dataset,
+            num_images_to_sample_from=self.config.eval_num_images_to_sample_from,
+            num_times_to_repeat_images=self.config.eval_num_times_to_repeat_images,
+            device=self.device,
+            num_workers=self.world_size * 4,
+            pin_memory=True,
+            collate_fn=self.config.collate_fn,
+        )
+        self.iter_eval_image_dataloader = iter(self.eval_image_dataloader)
+        self.eval_pixel_sampler = self._get_pixel_sampler(self.eval_dataset, self.config.eval_num_rays_per_batch)
+        self.eval_camera_optimizer = self.config.camera_optimizer.setup(
+            num_cameras=self.eval_dataset.cameras.size, device=self.device
+        )
+        self.eval_ray_generator = RayGenerator(
+            self.eval_dataset.cameras.to(self.device),
+            self.eval_camera_optimizer,
+        )
+        # for loading full images
+        self.fixed_indices_eval_dataloader = FixedIndicesEvalDataloader(
+            input_dataset=self.eval_dataset,
+            device=self.device,
+            num_workers=self.world_size * 4,
+        )
+        self.eval_dataloader = RandIndicesEvalDataloader(
+            input_dataset=self.eval_dataset,
+            device=self.device,
+            num_workers=self.world_size * 4,
+        )
+
+    def next_train(self, step: int) -> Tuple[RayBundle, Dict]:
+        """Returns the next batch of data from the train dataloader."""
+        self.train_count += 1
+        image_batch = next(self.iter_train_image_dataloader)
+        assert self.train_pixel_sampler is not None
+        batch = self.train_pixel_sampler.sample(image_batch)
+        ray_indices = batch["indices"]
+        ray_bundle = self.train_ray_generator(ray_indices)
+        batch["clip"], clip_scale = self.clip_interpolator(ray_indices)
+        batch["dino"] = self.dino_dataloader(ray_indices)
+        ray_bundle.metadata["clip_scales"] = clip_scale
+        # assume all cameras have the same focal length and image width
+        ray_bundle.metadata["fx"] = self.train_dataset.cameras[0].fx.item()
+        ray_bundle.metadata["width"] = self.train_dataset.cameras[0].width.item()
+        return ray_bundle, batch
+
+    def next_eval(self, step: int) -> Tuple[RayBundle, Dict]:
+        """Returns the next batch of data from the eval dataloader."""
+        self.eval_count += 1
+        image_batch = next(self.iter_eval_image_dataloader)
+        assert self.eval_pixel_sampler is not None
+        batch = self.eval_pixel_sampler.sample(image_batch)
+        ray_indices = batch["indices"]
+        ray_bundle = self.eval_ray_generator(ray_indices)
+        return ray_bundle, batch
+
+    def next_eval_image(self, step: int) -> Tuple[int, RayBundle, Dict]:
+        for camera_ray_bundle, batch in self.eval_dataloader:
+            assert camera_ray_bundle.camera_indices is not None
+            image_idx = int(camera_ray_bundle.camera_indices[0, 0, 0])
+            return image_idx, camera_ray_bundle, batch
+        raise ValueError("No more eval images")
+
+    def get_train_rays_per_batch(self) -> int:
+        return self.config.train_num_rays_per_batch
+
+    def get_eval_rays_per_batch(self) -> int:
+        return self.config.eval_num_rays_per_batch
+
+    def get_datapath(self) -> Path:
+        return self.config.dataparser.data
+
+    def get_param_groups(self) -> Dict[str, List[Parameter]]:  # pylint: disable=no-self-use
+        """Get the param groups for the data manager.
+        Returns:
+            A list of dictionaries containing the data manager's param groups.
+        """
+        param_groups = {}
+
+        camera_opt_params = list(self.train_camera_optimizer.parameters())
+        if self.config.camera_optimizer.mode != "off":
+            assert len(camera_opt_params) > 0
+            param_groups[self.config.camera_optimizer.param_group] = camera_opt_params
+        else:
+            assert len(camera_opt_params) == 0
+
+        return param_groups
