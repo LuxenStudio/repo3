9c2d85c1b022339657e9e3d79736fa1a61e69586
luxemstudio <kalijunaidtest@gmail.com>
2024-12-28 14:04:56 -0700

    Add completed training state (#1768)

--- luxenstudio/viewer/viser/messages.py
+++ luxenstudio/viewer/viser/messages.py
@@ -171,10 +171,10 @@ class DatasetImageMessage(LuxenstudioMessage):
 
 
 @dataclasses.dataclass
-class IsTrainingMessage(LuxenstudioMessage):
+class TrainingStateMessage(LuxenstudioMessage):
     """Wheather the scene is in training mode or not."""
 
-    is_training: bool
+    training_state: Literal["training", "paused", "completed"]
     """True if the model is currently trianing, False otherwise"""
 
 
