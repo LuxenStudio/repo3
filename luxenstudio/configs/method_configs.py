41df62abc6921384809b743f1b7c2c813d9d13cb
luxemstudio <kalijunaidtest@gmail.com>
2025-01-04 12:52:09 +0000

    merging

--- luxenstudio/configs/method_configs.py
+++ luxenstudio/configs/method_configs.py
@@ -24,21 +24,9 @@ import tyro
 
 from luxenstudio.cameras.camera_optimizers import CameraOptimizerConfig
 from luxenstudio.configs.base_config import ViewerConfig
-<<<<<<< HEAD
-from luxenstudio.data.datamanagers.base_datamanager import VanillaDataManagerConfig
-from luxenstudio.data.datamanagers.depth_datamanager import DepthDataManagerConfig
-from luxenstudio.data.datamanagers.sdf_datamanager import SDFDataManagerConfig
-from luxenstudio.data.datamanagers.dreamfusion_datamanager import (
-    DreamFusionDataManagerConfig,
-)
-from luxenstudio.data.datamanagers.semantic_datamanager import SemanticDataManagerConfig
-from luxenstudio.data.datamanagers.variable_res_datamanager import (
-    VariableResDataManagerConfig,
-=======
 from luxenstudio.data.datamanagers.base_datamanager import (
     VanillaDataManager,
     VanillaDataManagerConfig,
->>>>>>> b1ddb9e695b789fae47e0f760a5c7f59f25af0bb
 )
 from luxenstudio.data.dataparsers.blender_dataparser import BlenderDataParserConfig
 from luxenstudio.data.dataparsers.dluxen_dataparser import DLuxenDataParserConfig
@@ -59,10 +47,7 @@ from luxenstudio.engine.optimizers import AdamOptimizerConfig, RAdamOptimizerCon
 from luxenstudio.engine.schedulers import (
     CosineDecaySchedulerConfig,
     ExponentialDecaySchedulerConfig,
-<<<<<<< HEAD
-=======
     MultiStepSchedulerConfig,
->>>>>>> b1ddb9e695b789fae47e0f760a5c7f59f25af0bb
 )
 from luxenstudio.engine.trainer import TrainerConfig
 from luxenstudio.field_components.temporal_distortions import TemporalDistortionKind
@@ -75,10 +60,7 @@ from luxenstudio.models.luxenacto import LuxenactoModelConfig
 from luxenstudio.models.luxenplayer_luxenacto import LuxenplayerLuxenactoModelConfig
 from luxenstudio.models.luxenplayer_ngp import LuxenplayerNGPModelConfig
 from luxenstudio.models.neus import NeuSModelConfig
-<<<<<<< HEAD
-=======
 from luxenstudio.models.neus_facto import NeuSFactoModelConfig
->>>>>>> b1ddb9e695b789fae47e0f760a5c7f59f25af0bb
 from luxenstudio.models.semantic_luxenw import SemanticLuxenWModelConfig
 from luxenstudio.models.tensorf import TensoRFModelConfig
 from luxenstudio.models.vanilla_luxen import LuxenModel, VanillaModelConfig
@@ -103,10 +85,7 @@ descriptions = {
     "luxenplayer-luxenacto": "LuxenPlayer with luxenacto backbone.",
     "luxenplayer-ngp": "LuxenPlayer with InstantNGP backbone.",
     "neus": "Implementation of NeuS. (slow)",
-<<<<<<< HEAD
-=======
     "neus-facto": "Implementation of NeuS-Facto. (slow)",
->>>>>>> b1ddb9e695b789fae47e0f760a5c7f59f25af0bb
 }
 
 method_configs["luxenacto"] = TrainerConfig(
@@ -475,54 +454,8 @@ method_configs["dreamfusion"] = TrainerConfig(
     max_num_iterations=30000,
     mixed_precision=True,
     pipeline=VanillaPipelineConfig(
-<<<<<<< HEAD
-        generative=True,
-        datamanager=DreamFusionDataManagerConfig(
-            horizontal_rotation_warmup=4000,
-        ),
-        model=DreamFusionModelConfig(
-            eval_num_rays_per_chunk=1 << 15,
-            distortion_loss_mult=0.02,
-            interlevel_loss_mult=100.0,
-            orientation_loss_mult=0.1,
-            max_res=256,
-            sphere_collider=True,
-            initialize_density=False,
-            random_background=True,
-            proposal_warmup=500,
-            proposal_update_every=0,
-            proposal_weights_anneal_max_num_iters=100,
-            start_lambertian_training=500,
-            start_normals_training=1000,
-            opacity_loss_mult=0.001,
-            positional_prompting="discrete",
-            guidance_scale=100,
-        ),
-    ),
-    optimizers={
-        "proposal_networks": {
-            "optimizer": AdamOptimizerConfig(lr=1e-2, eps=1e-15),
-            "scheduler": None,
-        },
-        "fields": {
-            "optimizer": AdamOptimizerConfig(lr=1e-3, eps=1e-15),
-            "scheduler": ExponentialDecaySchedulerConfig(
-                warmup_steps=2000, lr_final=1e-6, max_steps=20000, ramp="linear"
-            ),
-        },
-    },
-    viewer=ViewerConfig(num_rays_per_chunk=1 << 15),
-    vis="viewer",
-)
-
-method_configs["luxenplayer-luxenacto"] = TrainerConfig(
-    method_name="luxenplayer-luxenacto",
-    pipeline=VanillaPipelineConfig(
-        datamanager=DepthDataManagerConfig(
-=======
         datamanager=VanillaDataManagerConfig(
             _target=VanillaDataManager[DepthDataset],
->>>>>>> b1ddb9e695b789fae47e0f760a5c7f59f25af0bb
             dataparser=DycheckDataParserConfig(),
             train_num_rays_per_batch=4096,
             eval_num_rays_per_batch=4096,
@@ -586,12 +519,8 @@ method_configs["neus"] = TrainerConfig(
     max_num_iterations=100000,
     mixed_precision=False,
     pipeline=VanillaPipelineConfig(
-<<<<<<< HEAD
-        datamanager=SDFDataManagerConfig(
-=======
         datamanager=VanillaDataManagerConfig(
             _target=VanillaDataManager[SDFDataset],
->>>>>>> b1ddb9e695b789fae47e0f760a5c7f59f25af0bb
             dataparser=SDFStudioDataParserConfig(),
             train_num_rays_per_batch=1024,
             eval_num_rays_per_batch=1024,
@@ -615,8 +544,6 @@ method_configs["neus"] = TrainerConfig(
     vis="viewer",
 )
 
-<<<<<<< HEAD
-=======
 method_configs["neus-facto"] = TrainerConfig(
     method_name="neus-facto",
     steps_per_eval_image=5000,
@@ -668,7 +595,6 @@ method_configs["neus-facto"] = TrainerConfig(
     vis="viewer",
 )
 
->>>>>>> b1ddb9e695b789fae47e0f760a5c7f59f25af0bb
 external_methods, external_descriptions = discover_methods()
 all_methods = {**method_configs, **external_methods}
 all_descriptions = {**descriptions, **external_descriptions}
