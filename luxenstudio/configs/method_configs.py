0fb9947626ec45c23ea0c1fa36d76bea95f65bfc
luxemstudio <kalijunaidtest@gmail.com>
2025-01-09 01:29:10 +0000

    revert luxenacto config change

--- luxenstudio/configs/method_configs.py
+++ luxenstudio/configs/method_configs.py
@@ -93,7 +93,7 @@ descriptions = {
 
 method_configs["luxenacto"] = TrainerConfig(
     method_name="luxenacto",
-    steps_per_eval_batch=20,
+    steps_per_eval_batch=500,
     steps_per_save=2000,
     max_num_iterations=30000,
     mixed_precision=True,
