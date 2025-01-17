9ccfc54106afb59fea7e9876ab16629a2c10f08c
luxemstudio <kalijunaidtest@gmail.com>
2025-01-17 14:53:08 +0000

    delete sample_config.py + small fixes

--- luxenstudio/configs/method_configs.py
+++ luxenstudio/configs/method_configs.py
@@ -22,6 +22,7 @@ from collections import OrderedDict
 from typing import Dict
 
 import tyro
+from luxenstudio.data.pixel_samplers import PairPixelSamplerConfig
 
 from luxenstudio.cameras.camera_optimizers import CameraOptimizerConfig
 from luxenstudio.configs.base_config import ViewerConfig
@@ -171,6 +172,7 @@ method_configs["depth-luxenacto"] = TrainerConfig(
     pipeline=VanillaPipelineConfig(
         datamanager=VanillaDataManagerConfig(
             _target=VanillaDataManager[DepthDataset],
+            sampling_config=PairPixelSamplerConfig(),
             dataparser=LuxenstudioDataParserConfig(),
             train_num_rays_per_batch=4096,
             eval_num_rays_per_batch=4096,
