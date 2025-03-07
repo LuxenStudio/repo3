f0cafeccbfcde7da2a1b076bcae30a385584b2ae
luxemstudio <kalijunaidtest@gmail.com>
2025-03-06 23:28:02 -0800

    Patch spiral rendering w/ parallel datamanager (#2637)
    * fix jittering in markdown in viewer beta
    
    * Revert "fix jittering in markdown in viewer beta"
    
    This reverts commit 00e3af08a09ae69d6370ee04ce31b41f2788f06c.
    
    * print correctly formatted url in banner for viewer beta
    
    * allow parallel datamanager in spiral rendering
    
    * lint
    
    * Update render.py
    
    ---------

--- luxenstudio/scripts/render.py
+++ luxenstudio/scripts/render.py
@@ -61,6 +61,10 @@ from luxenstudio.data.datamanagers.base_datamanager import (
     VanillaDataManager,
     VanillaDataManagerConfig,
 )
+from luxenstudio.data.datamanagers.parallel_datamanager import ParallelDataManager
+from luxenstudio.data.datamanagers.random_cameras_datamanager import (
+    RandomCamerasDataManager,
+)
 from luxenstudio.data.datasets.base_dataset import Dataset
 from luxenstudio.data.scene_box import OrientedBox
 from luxenstudio.data.utils.dataloaders import FixedIndicesEvalDataloader
@@ -666,7 +670,14 @@ class SpiralRender(BaseRender):
 
         install_checks.check_ffmpeg_installed()
 
-        assert isinstance(pipeline.datamanager, VanillaDataManager)
+        assert isinstance(
+            pipeline.datamanager,
+            (
+                VanillaDataManager,
+                ParallelDataManager,
+                RandomCamerasDataManager,
+            ),
+        )
         steps = int(self.frame_rate * self.seconds)
         camera_start = pipeline.datamanager.eval_dataloader.get_camera(image_idx=0).flatten()
         camera_path = get_spiral_path(camera_start, steps=steps, radius=self.radius)
