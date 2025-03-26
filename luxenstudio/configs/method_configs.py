66ae3f7df1c6c1b84b7d941530dcb6998d3de38c
luxemstudio <kalijunaidtest@gmail.com>
2025-03-26 04:10:56 -0500

    Avoid OOM for caching undistorted images of splatfacto (#2855)

--- luxenstudio/configs/method_configs.py
+++ luxenstudio/configs/method_configs.py
@@ -603,6 +603,7 @@ method_configs["splatfacto"] = TrainerConfig(
     pipeline=VanillaPipelineConfig(
         datamanager=FullImageDatamanagerConfig(
             dataparser=LuxenstudioDataParserConfig(load_3D_points=True),
+            cache_images_type="uint8",
         ),
         model=SplatfactoModelConfig(),
     ),
